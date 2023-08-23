a4bb8f7dae9385ba388d4d599ebc7287c64f5899
Aynuo <10zerobot@gmail.com>
2023-08-23 22:03:34 +0330

    add zip function and GUI

--- compress file application/main.py
+++ compress file application/main.py
@@ -1,2 +1,20 @@
-# write your codes here :)
-# start
+import PySimpleGUI as pg
+from zip import make_arc
+
+label_1 = pg.Text("Select Your Files TO Compress: ")
+file_path = pg.Input()
+choose_file_button = pg.FileBrowse("choose", key="files")
+
+label_2 = pg.Text("Select Your OutPut Directory:     ")
+file_output_dir = pg.Input()
+output_file_button = pg.FolderBrowse("choose", key="folder")
+
+compress_btn = pg.Button("Compress")
+output_label = pg.Text(key="output")
+
+win = pg.Window("file Compressor", layout=[[label_1,file_path,choose_file_button],
+                                            [label_2,file_output_dir,output_file_button],
+                                            [compress_btn,output_label]])
+
+win.read()
+win.close()
---
+++ compress file application/zip.py
@@ -0,0 +1,8 @@
+import zipfile as zf
+import pathlib
+
+def make_arc(filepaths, dst_dir):
+    dst_path = pathlib.Path(dst_dir, "zip_file.zip")
+    with az.ZipFile in filepaths:
+        filepath = pathlib.Path(filepath)
+        archive.write(filepath, arcname=filepath.name)
