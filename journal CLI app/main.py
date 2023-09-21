b8531461f006a4133e2c1d45963444cc65ecd571
Aynuo <10zerobot@gmail.com>
2023-09-21 16:59:25 +0330

    app completed

--- journal CLI app/main.py
+++ journal CLI app/main.py
@@ -2,6 +2,6 @@ date = input("Enter Today's Date? ")
 mood = input("How do you rate your mood today from 1 to 10? ")
 journal = input("Let your thoughts flow:\n ")
 
-with open(f"journals\{date}.txt", mode="w") as file:
-    file.write(mood)
+with open(f"journals/{date}.txt", mode="w") as file:
+    file.write(mood + ("\n"*2))
     file.write(journal)
