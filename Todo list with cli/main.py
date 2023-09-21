4d0ede4590d35b95864be91630841c050f17afdc
Aynuo <10zerobot@gmail.com>
2023-09-21 16:37:20 +0330

    add complete and edit feature

--- Todo list with cli/main.py
+++ Todo list with cli/main.py
@@ -13,8 +13,10 @@ while True:
 
         case "edit":
             number = int(input("enter your todo number for edit>> ")) - 1
-            new_todo = input("enter a new todo: ")
+            new_todo = input("enter a new todo: ") + "\n"
             todos[number] = new_todo
+            with open("todos.txt", "w") as write_edit:
+                write_edit.writelines(todos)
 
         case "show":
             new_todos = [item.strip("\n") for item in todos]
@@ -25,6 +27,8 @@ while True:
         case "complete":
             done_number = int(input("enter your todo number to delete:")) - 1
             todos.pop(done_number)
+            with open("todos.txt", "w") as write_complete:
+                write_complete.writelines(todos)
 
         case "exit":
             break
