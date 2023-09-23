d126ec633ad21dfec8c8fdd1593e017905a23346
Aynuo <10zerobot@gmail.com>
2023-09-23 18:23:09 +0330

    new project: password checker

---
+++ password checker/main.py
@@ -0,0 +1,27 @@
+# give an input
+password = input("Enter you password to check => ")
+
+# result
+result = []
+
+# check password length
+if len(password) > 8:
+    result.append("True")
+else:
+    result.append("False")
+
+# check for include number
+is_digit = False
+for char in password:
+    if char.isdigit():
+        is_digit = True
+result.append(is_digit)
+
+# check for include uppercase
+is_upper = False
+for char in password:
+    if char.isupper():
+        is_upper = True
+result.append(is_upper)
+
+print(all(result))
