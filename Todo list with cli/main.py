bfe779ca328257388281c042bf82498023f5561b
Aynuo <10zerobot@gmail.com>
2023-09-27 16:52:11 +0330

    comment added

--- Todo list with cli/main.py
+++ Todo list with cli/main.py
@@ -1,15 +1,19 @@
+# open todos txt file
 with open("todos.txt", "r") as file:
     todos = file.readlines()
 
 while True:
+    # ask for operation
     user_action = input("--enter add, show, edit, complete or exit--? ").strip(" ")
 
+    # add operation
     if user_action.startswith("add"):
         todo = user_action[4:] + "\n"
         todos.append(todo)
         with open("todos.txt", "w") as write_file:
             write_file.writelines(todos)
 
+    # edit operation
     elif user_action.startswith("edit"):
         try:
             number = int(user_action[5]) - 1
@@ -21,12 +25,14 @@ while True:
             print("invalid command")
             continue
 
+    # show operation
     elif user_action.startswith("show"):
         new_todos = [item.strip("\n") for item in todos]
         for index, item in enumerate(new_todos):
             index = index + 1
             print(f"{index} => {item}")
 
+    # complete operation
     elif user_action.startswith("complete"):
         try:
             done_number = int(user_action[9]) - 1
@@ -37,6 +43,7 @@ while True:
             print("There is no item with this number.")
             continue
 
+    # exit operation
     elif user_action.startswith("exit"):
         break
 
